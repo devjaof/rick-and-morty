3bcc42e9cf81b6308ee6e1e1cab15b5f812508cb
Joao Francisco <joao.rodrigues@viasoft.com.br>
2022-06-29 10:55:56 -0300

    pickle riiiiick

--- app/pods/index/controller.js
+++ app/pods/index/controller.js
@@ -3,8 +3,9 @@ import { task } from 'ember-concurrency';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 export default class IndexController extends Controller {
+  
   @task
-  *load3d(){
+  *load3D(){
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
